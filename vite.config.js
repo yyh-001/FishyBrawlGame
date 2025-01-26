967aec14732d848de77e41483958453a414d5083
yyh <1807875605@qq.com>
2025-01-26 11:36:36 +0000

    修改后台接口读取bug

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,42 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  server: {
-    host: "0.0.0.0",
-    port: "3001",
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_BASE_URL,
-        changeOrigin: true,
-        secure: false
+export default defineConfig(({ mode }) => {
+  // 加载环境变量
+  const env = loadEnv(mode, process.cwd())
+  
+  return {
+    plugins: [
+      vue(),
+    ],
+    server: {
+      host: "0.0.0.0",
+      port: "3001",
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          secure: false
+        }
+      },
+      allowedHosts: [
+        'zdvstegdxdfm.sealosbja.site',
+        'xkmvwivzjdqv.sealosbja.site',
+        'xfpeuhaouomq.sealosbja.site'
+      ]
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
     },
-    allowedHosts: [
-      'zdvstegdxdfm.sealosbja.site',
-      'xkmvwivzjdqv.sealosbja.site',
-      'xfpeuhaouomq.sealosbja.site'
-    ]
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "@/assets/styles/variables" as *;`
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@/assets/styles/variables" as *;`
+        }
       }
     }
   }
